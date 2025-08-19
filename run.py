243dc026c86ad8b27bca985262aaa91f01a9daba
bongjooncha <cbj4133@naver.com>
2025-08-19 10:48:00 +0900

    data migration

---
+++ run.py
@@ -0,0 +1,25 @@
+import time
+import asyncio
+import schedule
+from scripts.daily import daily_task
+
+async def job(n):
+    await daily_task(n)
+
+def run_job(n):
+    asyncio.run(job(n))
+    print(f"크롤링 시간: {time.strftime("%Y-%m-%d %H:%M:%S")}")
+
+run_job(n = 2)
+
+# 매일 아침 6시에 실행하도록 스케줄 설정
+schedule.every().day.at("06:00").do(run_job, n = 2)
+
+def main():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main() 
