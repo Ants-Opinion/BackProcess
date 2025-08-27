d0311898fbb59c652bd2d90e9640aef0c6894bfa
bongjooncha <cbj4133@naver.com>
2025-08-27 15:15:25 +0900

    python3.13.2에 의한 변환

--- run.py
+++ run.py
@@ -8,9 +8,9 @@ async def job(n):
 
 def run_job(n):
     asyncio.run(job(n))
-    print(f"크롤링 시간: {time.strftime("%Y-%m-%d %H:%M:%S")}")
+    print(f"크롤링 시간: {time.strftime('%Y-%m-%d %H:%M:%S')}")
 
-run_job(n = 2)
+run_job(n = 7)
 
 # 매일 아침 6시에 실행하도록 스케줄 설정
 schedule.every().day.at("06:00").do(run_job, n = 2)
@@ -22,4 +22,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main() 
+    main()
